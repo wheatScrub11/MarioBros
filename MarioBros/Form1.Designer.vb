5d969adf342ba8cd920e3018dbecdb624e8cc8c7
Nicolas Baez Chavez <nicbez@soy.sena.edu.co>
2024-04-30 18:54:31 -0500

    Primeras pruebas movimiento + colisiones

--- MarioBros/Form1.Designer.vb
+++ MarioBros/Form1.Designer.vb
@@ -22,16 +22,85 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pb1 = New System.Windows.Forms.PictureBox()
+        Me.pb2 = New System.Windows.Forms.PictureBox()
+        Me.timer_moveRight = New System.Windows.Forms.Timer(Me.components)
+        Me.timer_moveLeft = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.timer_gravity = New System.Windows.Forms.Timer(Me.components)
+        Me.timer_jump = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'pb1
+        '
+        Me.pb1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.pb1.Location = New System.Drawing.Point(476, 104)
+        Me.pb1.Name = "pb1"
+        Me.pb1.Size = New System.Drawing.Size(100, 100)
+        Me.pb1.TabIndex = 0
+        Me.pb1.TabStop = False
+        '
+        'pb2
+        '
+        Me.pb2.BackColor = System.Drawing.Color.Tomato
+        Me.pb2.Location = New System.Drawing.Point(655, 296)
+        Me.pb2.Name = "pb2"
+        Me.pb2.Size = New System.Drawing.Size(100, 98)
+        Me.pb2.TabIndex = 1
+        Me.pb2.TabStop = False
+        '
+        'timer_moveRight
+        '
+        Me.timer_moveRight.Interval = 1
+        '
+        'timer_moveLeft
+        '
+        Me.timer_moveLeft.Interval = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.MediumSeaGreen
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 400)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(923, 92)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'timer_gravity
+        '
+        Me.timer_gravity.Interval = 1
+        '
+        'timer_jump
+        '
+        Me.timer_jump.Interval = 10
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(924, 491)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.pb2)
+        Me.Controls.Add(Me.pb1)
+        Me.KeyPreview = True
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents pb1 As PictureBox
+    Friend WithEvents pb2 As PictureBox
+    Friend WithEvents timer_moveRight As Timer
+    Friend WithEvents timer_moveLeft As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents timer_gravity As Timer
+    Friend WithEvents timer_jump As Timer
 End Class
--- MarioBros/Form1.vb
+++ MarioBros/Form1.vb
@@ -1,3 +1,130 @@
 ﻿Public Class Form1
 
+    Dim yAxeGroundPosition As Int64
+    Public Function applyGravity() As Boolean
+        If (pb1.Location.Y + 100) >= yAxeGroundPosition Then ''when reaches the ground
+            System.Console.WriteLine("XDD")
+            timer_gravity.Stop()
+        Else
+            pb1.Location = New Point(pb1.Location.X, pb1.Location.Y + 10)
+        End If
+
+        Return False
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        yAxeGroundPosition = 400
+        timer_gravity.Start()
+    End Sub
+
+
+    Private Sub Form1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        ' Sets Handled to true to prevent other controls from
+        ' receiving the key if an arrow key was pressed
+
+
+
+        Dim bHandled As Boolean = False
+        System.Console.WriteLine(e.KeyData)
+        Select Case e.KeyCode
+            Case Keys.Right
+                enableXAxesCubeMovement("right")
+                e.Handled = True
+            Case Keys.Left
+                enableXAxesCubeMovement("left")
+                e.Handled = True
+            Case Keys.Up
+
+            Case Keys.Down
+
+
+
+        End Select
+    End Sub
+
+    Private Sub Form1_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp
+        ' Sets Handled to true to prevent other controls from
+        ' receiving the key if an arrow key was pressed
+        Dim bHandled As Boolean = False
+        If e.KeyCode = Keys.Right Then
+
+            cleanXAxesCubeMovement("right")
+        ElseIf e.KeyCode = Keys.Left Then
+
+            cleanXAxesCubeMovement("left")
+
+        ElseIf e.KeyCode = Keys.Up Then
+
+
+
+        End If
+
+    End Sub
+
+    Public Function enableXAxesCubeMovement(direction As String) As Boolean
+
+        If direction = "right" Then
+            timer_moveRight.Start()
+        ElseIf direction = "left" Then
+            timer_moveLeft.Start()
+        End If
+        Return False
+    End Function
+
+    Public Function cleanXAxesCubeMovement(direction As String) As Boolean
+        If direction = "right" Then
+            timer_moveRight.Stop()
+        ElseIf direction = "left" Then
+            timer_moveLeft.Stop()
+        End If
+        Return False
+    End Function
+
+
+
+    Private Sub timer_moveRight_Tick(sender As Object, e As EventArgs) Handles timer_moveRight.Tick
+        System.Console.WriteLine("---------------")
+        System.Console.WriteLine(pb1.Location.X + 100)
+        System.Console.WriteLine(pb2.Left)
+        System.Console.WriteLine("---------------")
+
+        If pb1.Location.X + 100 = pb2.Left Then
+            pb1.Location = New Point(pb1.Location.X, pb1.Location.Y)
+        Else
+            System.Console.WriteLine("PASO")
+            If timer_moveRight.Enabled = True And timer_moveLeft.Enabled = True Then
+                pb1.Location = New Point(pb1.Location.X, pb1.Location.Y)
+            Else
+                pb1.Location = New Point(pb1.Location.X + 1, pb1.Location.Y)
+            End If
+        End If
+
+
+    End Sub
+    Private Sub timer_moveLeft_Tick(sender As Object, e As EventArgs) Handles timer_moveLeft.Tick
+
+        If timer_moveRight.Enabled = True And timer_moveLeft.Enabled = True Then
+            pb1.Location = New Point(pb1.Location.X, pb1.Location.Y)
+        Else
+            pb1.Location = New Point(pb1.Location.X - 15, pb1.Location.Y)
+        End If
+
+
+    End Sub
+    Private Sub timer_jump_Tick(sender As Object, e As EventArgs) Handles timer_jump.Tick
+
+    End Sub
+
+    Private Sub pb1_LocationChanged(sender As Object, e As EventArgs) Handles pb1.LocationChanged
+        'If pb1.Bounds.IntersectsWith(pb2.Bounds) Then
+        'pb1.Location = New Point(pb1.Location.X - 1, pb1.Location.Y)
+        'End If
+    End Sub
+
+    Private Sub timer_gravity_Tick(sender As Object, e As EventArgs) Handles timer_gravity.Tick
+        applyGravity()
+    End Sub
+
+
+
+
 End Class
