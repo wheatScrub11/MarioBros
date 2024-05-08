25240507ccd7c06b7a11d8fb4cd34e82eaf19a51
Nicolas Baez Chavez <nicbez@soy.sena.edu.co>
2024-05-07 21:22:58 -0500

    Implementación salto y caída en plataformas

--- MarioBros/Form1.Designer.vb
+++ MarioBros/Form1.Designer.vb
@@ -31,18 +31,34 @@ Partial Class Form1
         Me.timer_jump = New System.Windows.Forms.Timer(Me.components)
         Me.col = New System.Windows.Forms.PictureBox()
         Me.col2 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.col3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
         CType(Me.pb1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.col, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.col2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.col3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'pb1
@@ -50,7 +66,7 @@ Partial Class Form1
         Me.pb1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.pb1.Location = New System.Drawing.Point(295, 224)
         Me.pb1.Name = "pb1"
-        Me.pb1.Size = New System.Drawing.Size(100, 100)
+        Me.pb1.Size = New System.Drawing.Size(50, 50)
         Me.pb1.TabIndex = 0
         Me.pb1.TabStop = False
         '
@@ -82,69 +98,157 @@ Partial Class Form1
         'col
         '
         Me.col.BackColor = System.Drawing.Color.SandyBrown
-        Me.col.Location = New System.Drawing.Point(395, 224)
+        Me.col.Location = New System.Drawing.Point(345, 224)
         Me.col.Name = "col"
-        Me.col.Size = New System.Drawing.Size(5, 98)
+        Me.col.Size = New System.Drawing.Size(5, 48)
         Me.col.TabIndex = 4
         Me.col.TabStop = False
         '
         'col2
         '
         Me.col2.BackColor = System.Drawing.Color.SandyBrown
-        Me.col2.Location = New System.Drawing.Point(290, 224)
+        Me.col2.Location = New System.Drawing.Point(290, 225)
         Me.col2.Name = "col2"
-        Me.col2.Size = New System.Drawing.Size(5, 98)
+        Me.col2.Size = New System.Drawing.Size(5, 48)
         Me.col2.TabIndex = 5
         Me.col2.TabStop = False
-        '
-        'PictureBox4
-        '
-        Me.PictureBox4.BackColor = System.Drawing.Color.Tomato
-        Me.PictureBox4.Location = New System.Drawing.Point(741, 237)
-        Me.PictureBox4.Name = "PictureBox4"
-        Me.PictureBox4.Size = New System.Drawing.Size(100, 100)
-        Me.PictureBox4.TabIndex = 7
-        Me.PictureBox4.TabStop = False
-        Me.PictureBox4.Tag = "walls"
         '
         'PictureBox2
         '
         Me.PictureBox2.BackColor = System.Drawing.Color.Tomato
-        Me.PictureBox2.Location = New System.Drawing.Point(103, 271)
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 324)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(100, 100)
+        Me.PictureBox2.Size = New System.Drawing.Size(40, 70)
         Me.PictureBox2.TabIndex = 8
         Me.PictureBox2.TabStop = False
         Me.PictureBox2.Tag = "walls"
         '
-        'PictureBox3
-        '
-        Me.PictureBox3.BackColor = System.Drawing.Color.Tomato
-        Me.PictureBox3.Location = New System.Drawing.Point(520, 271)
-        Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(100, 100)
-        Me.PictureBox3.TabIndex = 9
-        Me.PictureBox3.TabStop = False
-        Me.PictureBox3.Tag = "walls"
-        '
         'col3
         '
         Me.col3.BackColor = System.Drawing.Color.SandyBrown
-        Me.col3.Location = New System.Drawing.Point(296, 324)
+        Me.col3.Location = New System.Drawing.Point(296, 274)
         Me.col3.Name = "col3"
-        Me.col3.Size = New System.Drawing.Size(98, 5)
+        Me.col3.Size = New System.Drawing.Size(48, 10)
         Me.col3.TabIndex = 10
         Me.col3.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox3.Location = New System.Drawing.Point(207, 113)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(40, 281)
+        Me.PictureBox3.TabIndex = 11
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Tag = "walls"
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox5.Location = New System.Drawing.Point(732, 225)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(40, 100)
+        Me.PictureBox5.TabIndex = 13
+        Me.PictureBox5.TabStop = False
+        Me.PictureBox5.Tag = "walls"
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox6.Location = New System.Drawing.Point(839, 173)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(40, 100)
+        Me.PictureBox6.TabIndex = 14
+        Me.PictureBox6.TabStop = False
+        Me.PictureBox6.Tag = "walls"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox4.Location = New System.Drawing.Point(534, 324)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(40, 100)
+        Me.PictureBox4.TabIndex = 15
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Tag = "walls"
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox7.Location = New System.Drawing.Point(629, 262)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(40, 100)
+        Me.PictureBox7.TabIndex = 16
+        Me.PictureBox7.TabStop = False
+        Me.PictureBox7.Tag = "walls"
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox8.Location = New System.Drawing.Point(395, 113)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(187, 37)
+        Me.PictureBox8.TabIndex = 17
+        Me.PictureBox8.TabStop = False
+        Me.PictureBox8.Tag = "walls"
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox9.Location = New System.Drawing.Point(161, 281)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(40, 37)
+        Me.PictureBox9.TabIndex = 18
+        Me.PictureBox9.TabStop = False
+        Me.PictureBox9.Tag = "walls"
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox10.Location = New System.Drawing.Point(12, 224)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(40, 19)
+        Me.PictureBox10.TabIndex = 19
+        Me.PictureBox10.TabStop = False
+        Me.PictureBox10.Tag = "walls"
+        '
+        'PictureBox11
+        '
+        Me.PictureBox11.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox11.Location = New System.Drawing.Point(694, 125)
+        Me.PictureBox11.Name = "PictureBox11"
+        Me.PictureBox11.Size = New System.Drawing.Size(47, 25)
+        Me.PictureBox11.TabIndex = 20
+        Me.PictureBox11.TabStop = False
+        Me.PictureBox11.Tag = "walls"
+        '
+        'PictureBox12
+        '
+        Me.PictureBox12.BackColor = System.Drawing.Color.Tomato
+        Me.PictureBox12.Location = New System.Drawing.Point(161, 173)
+        Me.PictureBox12.Name = "PictureBox12"
+        Me.PictureBox12.Size = New System.Drawing.Size(40, 19)
+        Me.PictureBox12.TabIndex = 21
+        Me.PictureBox12.TabStop = False
+        Me.PictureBox12.Tag = "walls"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(924, 491)
-        Me.Controls.Add(Me.col3)
-        Me.Controls.Add(Me.PictureBox3)
-        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox12)
+        Me.Controls.Add(Me.PictureBox11)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
         Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.col3)
+        Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.col2)
         Me.Controls.Add(Me.col)
         Me.Controls.Add(Me.PictureBox1)
@@ -156,10 +260,18 @@ Partial Class Form1
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.col, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.col2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.col3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -172,8 +284,16 @@ Partial Class Form1
     Friend WithEvents timer_jump As Timer
     Friend WithEvents col As PictureBox
     Friend WithEvents col2 As PictureBox
-    Friend WithEvents PictureBox4 As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
-    Friend WithEvents PictureBox3 As PictureBox
     Friend WithEvents col3 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
 End Class
--- MarioBros/Form1.vb
+++ MarioBros/Form1.vb
@@ -2,8 +2,13 @@ Public Class Form1
     Dim y As Integer = 300
     Dim velocityY As Double = 0
     Dim gravity As Double = 9.8
-    Dim jumpSpeed As Double = -500 ' Timer tick in seconds
+    Dim jumpSpeed As Double = -300
     Dim isJumping As Boolean = False
+
+    Dim yAxeGroundPosition As Int64
+    Dim moveVal As Int64 = 5
+    Dim moveRight As Boolean = True
+    Dim moveLeft As Boolean = True
 
     Private Sub timer_jump_Tick(sender As Object, e As EventArgs) Handles timer_jump.Tick
         If isJumping Then
@@ -13,40 +18,62 @@ Public Class Form1
             col2.Location = New Point(col2.Location.X, col2.Location.Y + velocityY * 0.02)
             col3.Location = New Point(col3.Location.X, col3.Location.Y + velocityY * 0.02)
 
-            If pb1.Location.Y >= 300 Then
+            Dim res As Tuple(Of Boolean, PictureBox) = checkGround(col3)
+            If res IsNot Nothing AndAlso res.Item1 = True = True Then
+                System.Console.WriteLine(res.Item2.Location.Y)
+                yAxeGroundPosition = res.Item2.Location.Y
+            End If
+
+            If pb1.Location.Y >= yAxeGroundPosition - 50 Then
+                movePlayerToPoint(pb1.Location.X, yAxeGroundPosition - 50)
+                movePlayerHitboxes(moveVal, "jump")
                 y = 400
                 velocityY = 0
                 isJumping = False
+                timer_gravity.Start()
+
             End If
         End If
 
 
     End Sub
 
+    Public Function checkGround(collider As PictureBox) As Tuple(Of Boolean, PictureBox)
+        Dim isCollision As Boolean = False
+        Dim collisionMatch As PictureBox = New PictureBox
 
-    Dim yAxeGroundPosition As Int64
-    Dim moveVal As Int64 = 5
-    Dim moveRight As Boolean = True
-    Dim moveLeft As Boolean = True
+        For Each ctrl As PictureBox In Me.Controls
+            If TypeOf ctrl Is PictureBox AndAlso ctrl.Tag = "walls" Then
+                Dim wall As PictureBox = DirectCast(ctrl, PictureBox)
+
+                If collider.Bounds.IntersectsWith(wall.Bounds) Then
+                    isCollision = True
+                    collisionMatch = wall
+                    Return Tuple.Create(isCollision, wall)
+                    'System.Console.WriteLine(wall.Location.Y)
+                    'yAxeGroundPosition = wall.Location.Y
+                End If
+            End If
+        Next
+    End Function
+
+
+
     Public Function applyGravity() As Boolean
-        If (pb1.Location.Y + 100) >= yAxeGroundPosition Then ''when reaches the ground
+        If (pb1.Location.Y + 50) >= yAxeGroundPosition Then ''when reaches the ground
             System.Console.WriteLine("XDD")
-            timer_gravity.Stop()
+            'timer_gravity.Stop()
         Else
-            pb1.Location = New Point(pb1.Location.X, pb1.Location.Y + 10)
+            pb1.Location = New Point(pb1.Location.X, pb1.Location.Y + 5)
             col.Location = New Point(col.Location.X, pb1.Location.Y)
             col2.Location = New Point(col2.Location.X, pb1.Location.Y)
             col3.Location = New Point(col3.Location.X, pb1.Location.Y + pb1.Width)
 
-            For Each ctrl As PictureBox In Me.Controls
-                If TypeOf ctrl Is PictureBox AndAlso ctrl.Tag = "walls" Then
-                    Dim wall As PictureBox = DirectCast(ctrl, PictureBox)
-                    If col3.Bounds.IntersectsWith(wall.Bounds) Then
-                        'yAxeGroundPosition = wall.Location.Y
-                    End If
-
-                End If
-            Next
+            Dim res As Tuple(Of Boolean, PictureBox) = checkGround(col3)
+            If res IsNot Nothing AndAlso res.Item1 = True = True Then
+                System.Console.WriteLine(res.Item2.Location.Y)
+                yAxeGroundPosition = res.Item2.Location.Y
+            End If
         End If
 
         Return False
@@ -71,6 +98,7 @@ Public Class Form1
                 e.Handled = True
             Case Keys.Up
                 If Not isJumping Then
+                    timer_gravity.Stop()
                     isJumping = True
                     velocityY = jumpSpeed
                 End If
@@ -144,53 +172,77 @@ Public Class Form1
             col2.Location = New Point(col2.Location.X - val, col2.Location.Y)
             col.Location = New Point(col.Location.X - val, col.Location.Y)
             col3.Location = New Point(col3.Location.X - val, col3.Location.Y)
+        ElseIf direction = "jump" Then
+            col.Location = New Point(col.Location.X, pb1.Location.Y + 1)
+            col2.Location = New Point(col2.Location.X, pb1.Location.Y + 1)
+            col3.Location = New Point(col3.Location.X, pb1.Location.Y + pb1.Width)
         End If
     End Function
 
+
+
     Public Function movePlayerToRight() As Boolean
+        Dim isThereCollision As Boolean
+        Dim collisionMatch As PictureBox
         For Each ctrl As Control In Me.Controls
             If TypeOf ctrl Is PictureBox AndAlso ctrl.Tag = "walls" Then
                 Dim wall As PictureBox = DirectCast(ctrl, PictureBox)
 
-                If moveRight Then
-                    If col.Bounds.IntersectsWith(wall.Bounds) Then
-                        Dim distance = wall.Location.X - (pb1.Location.X + pb1.Width)
-                        movePlayerToPoint(pb1.Location.X + distance, pb1.Location.Y)
-                        movePlayerHitboxes(distance, "right")
-                        moveRight = False
-
-                    Else
-                        movePlayerToPoint(pb1.Location.X + moveVal, pb1.Location.Y)
-                        movePlayerHitboxes(moveVal, "right")
-                        moveLeft = True
-
-                    End If
+                If col.Bounds.IntersectsWith(wall.Bounds) Then
+                    isThereCollision = True
+                    collisionMatch = wall
                 End If
+
+
             End If
         Next
+
+        If moveRight Then
+            If isThereCollision Then
+                Dim distance = collisionMatch.Location.X - (pb1.Location.X + pb1.Width)
+                movePlayerToPoint(pb1.Location.X + distance, pb1.Location.Y)
+                movePlayerHitboxes(distance, "right")
+                'moveRight = False
+
+            Else
+                movePlayerToPoint(pb1.Location.X + moveVal, pb1.Location.Y)
+                movePlayerHitboxes(moveVal, "right")
+                'moveLeft = True
+
+            End If
+        End If
     End Function
 
     Public Function movePlayerToLeft() As Boolean
+        Dim isThereCollision As Boolean
+        Dim collisionMatch As PictureBox
         For Each ctrl As Control In Me.Controls
             If TypeOf ctrl Is PictureBox AndAlso ctrl.Tag = "walls" Then
                 Dim wall As PictureBox = DirectCast(ctrl, PictureBox)
-                If moveLeft Then
-                    If col2.Bounds.IntersectsWith(wall.Bounds) Then
-                        Dim distance = pb1.Location.X - (wall.Location.X + wall.Width)
-                        movePlayerToPoint(pb1.Location.X - distance, pb1.Location.Y)
-                        movePlayerHitboxes(distance, "left")
-                        moveLeft = False
-
-                    Else
-                        movePlayerToPoint(pb1.Location.X - moveVal, pb1.Location.Y)
-                        movePlayerHitboxes(moveVal, "left")
-                        moveRight = True
-
-                    End If
+                If col2.Bounds.IntersectsWith(wall.Bounds) Then
+                    isThereCollision = True
+                    collisionMatch = wall
                 End If
 
             End If
         Next
+
+        If moveLeft Then
+            If isThereCollision Then
+                Dim distance = pb1.Location.X - (collisionMatch.Location.X + collisionMatch.Width)
+                movePlayerToPoint(pb1.Location.X - distance, pb1.Location.Y)
+                movePlayerHitboxes(distance, "left")
+                'moveLeft = False
+
+            Else
+                movePlayerToPoint(pb1.Location.X - moveVal, pb1.Location.Y)
+                movePlayerHitboxes(moveVal, "left")
+                'moveRight = True
+
+
+
+            End If
+        End If
     End Function
     Private Sub timer_moveLeft_Tick(sender As Object, e As EventArgs) Handles timer_moveLeft.Tick
 
@@ -235,6 +287,24 @@ Public Class Form1
         Return False
     End Function
 
+    Public Function kk3() As Boolean
+        Dim isCollision As Boolean = False
+        For Each ctrl As Control In Me.Controls
+            If TypeOf ctrl Is PictureBox AndAlso ctrl.Tag = "walls" Then
+                Dim wall As PictureBox = DirectCast(ctrl, PictureBox)
+
+                If col3.Bounds.IntersectsWith(wall.Bounds) Then
+                    isCollision = True
+                End If
+            End If
+        Next
+
+        If isCollision = False Then
+            yAxeGroundPosition = 400
+        End If
+        Return False
+    End Function
+
     Private Sub pb1_LocationChanged(sender As Object, e As EventArgs) Handles pb1.LocationChanged
         System.Console.WriteLine(moveRight)
         System.Console.WriteLine(moveLeft)
@@ -248,16 +318,13 @@ Public Class Form1
         abc()
     End Sub
 
-    Private Sub col2_Click(sender As Object, e As EventArgs) Handles col2.Click
-
-    End Sub
 
     Private Sub col2_LocationChanged(sender As Object, e As EventArgs) Handles col2.LocationChanged
         bca()
     End Sub
 
-    Private Sub col3_Click(sender As Object, e As EventArgs) Handles col3.Click
-
+    Private Sub col3_LocationChanged(sender As Object, e As EventArgs) Handles col3.LocationChanged
+        kk3()
     End Sub
 End Class
 
