51e4a1c1aa81b0b4d158e4d40a0d0898d17f2712
Ching <programaconching@gmail.com>
2024-05-20 12:56:29 -0500

    Inicio_formularios

--- MarioBros/Form2.vb
+++ MarioBros/Form2.vb
@@ -1,9 +1,13 @@
 ﻿Public Class FormularioBrayan
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
 
     End Sub
 
     Private Sub FormularioBrayan_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub FormularioBrayan_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
